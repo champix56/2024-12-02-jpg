f338c9813205287c51afabe835c762a8ea716bb1
champix56 <desorbaix@free.fr>
2024-12-04 17:52:01 +0100

    ajout d'un noeud de travail pour le routeur

--- js/index.js
+++ js/index.js
@@ -26,4 +26,4 @@ function loadNavbarEvents() {
   });
 }
 document.addEventListener("DOMContentLoaded", loadingDOM);
-var router=new Router();
+var router=new Router(document.getElementById('wrapper'));
