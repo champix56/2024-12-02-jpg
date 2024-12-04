af773e94fba554af6b6be48657d01cb9d8dac5b9
champix56 <desorbaix@free.fr>
2024-12-04 15:14:32 +0100

    ajout des event de preventdefault sur les liens de navbar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 /**
- * chargement de base de mon app 
+ * chargement de base de mon app
  */
 function loadingDOM() {
   document.querySelector("#js-notification").remove();
@@ -9,7 +9,18 @@ function loadingDOM() {
 /**
  * Fonction de chargement des events de navbar
  */
-function loadNavbarEvents(){
-
+function loadNavbarEvents() {
+  var aList = document.querySelectorAll("nav a");
+  // for(var i=0;i<aList.length;i++){
+  //   aList[i].addEventListener('click',function(evt){
+  //     evt.preventDefault();
+  //     console.log('navigate to ',evt.target.href);
+  //   })
+  // }
+  aList.forEach(function (element) {
+    element.addEventListener("click", function (evt) {
+      evt.preventDefault();
+    });
+  });
 }
 document.addEventListener("DOMContentLoaded", loadingDOM);
