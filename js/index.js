d8025821142f60bc3de603d47ba2b6c205056267
champix56 <desorbaix@free.fr>
2024-12-04 14:30:47 +0100

    destruction DOM innerHTML suite sur header

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 function loadingDOM() {
   document.querySelector("#js-notification").remove();
-  //document.querySelector("#header button").remove();
+  document.querySelector("#header button").remove();
   
   document.querySelector("#header button").addEventListener("click", function (evt) {
     console.log("header", evt);
     console.log("header clické");
   });
   /*destruction du dom et des events contenus dans le dom de #header avec innerHTML*/
-  document.querySelector("#header button").innerHTML+="Bla bla";
+  document.querySelector("#header").innerHTML+="Bla bla";
 }
 
 document.addEventListener("DOMContentLoaded", loadingDOM);
