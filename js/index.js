8dac1bf0d61afa435fa1211d5cd1c8314e5fe078
champix56 <desorbaix@free.fr>
2024-12-04 12:22:02 +0100

    propagation des events

--- js/index.js
+++ js/index.js
@@ -3,6 +3,18 @@ function loadingDOM() {
   js.style.backgroundColor = "GREEN";
   js.innerHTML =
     'le <span style="font-weight:900;color :blue;">JS</span> est OK';
+  document
+    .querySelector("button.btn-danger")
+    .addEventListener("click", function (evt) {
+      console.log('button',evt);
+      console.log("button du header clické");
+    });
+    document
+    .querySelector("#header")
+    .addEventListener("click", function (evt) {
+      console.log('header',evt);
+      console.log("header clické");
+    });
 }
 
-document.addEventListener('DOMContentLoaded',loadingDOM);
+document.addEventListener("DOMContentLoaded", loadingDOM);
