c68cd1f1808055dcc1003feb4c343e998f1c914c
champix56 <desorbaix@free.fr>
2024-12-04 17:31:30 +0100

    base xhr

--- js/router.js
+++ js/router.js
@@ -8,31 +8,57 @@ privé pas sur this
     +modif contenu(loadEvents)
     +recuperation du contenu de page depuis le reseau
 */
-function Router(rootFolderOfTemplates='/pages') {
+function Router(rootFolderOfTemplates = "/pages") {
   /*definitions locales(interne) des propriétés et fonctions */
   var currentRoute = location.pathname;
   function changePathName(pathName) {
-    history.pushState(null,null,pathName);
-    currentRoute=location.pathname;
+    history.pushState(null, null, pathName);
+    currentRoute = location.pathname;
   }
   function loadContentInPage(eventLoader) {}
-  function getContentFromNetwork(contentUrl) {}
+  function getContentFromNetwork(contentUrl) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", contentUrl);
+    xhr.onreadystatechange = function (evt) {
+      if (xhr.readyState < XMLHttpRequest.DONE) {
+        return;
+      }
+      if (xhr.status >= 400) {
+        console.log("erreur" + xhr.status);
+      }
+      console.log(xhr.responseText);
+    };
+  }
 
   /*definition des acces exterieurs a l'instance */
   /**
    * getter de la route current
    * @returns {string}current pathName
    */
-  this.getCurrentRoute=getCurrentRoute;
-  function getCurrentRoute(){return currentRoute;}
+  this.getCurrentRoute = getCurrentRoute;
+  function getCurrentRoute() {
+    return currentRoute;
+  }
 
   /**
    * fonction de navigation avec chargement du contenu
-   * @param {string} pathName path to navigate (start with '/') 
+   * @param {string} pathName path to navigate (start with '/')
    */
-  this.navigate=navigate;
-  function navigate(pathName='/'){
+  this.navigate = navigate;
+  function navigate(pathName = "/") {
     changePathName(pathName);
+    var url = rootFolderOfTemplates;
+    switch (pathName) {
+      case "/thumbnail":
+        url += "/thumbnail/thumbnail.html";
+        break;
+      case "/editor":
+        url += "/editor/editor.html";
+        break;
+      default:
+        url += "/home/home.html";
+        break;
+    }
     getContentFromNetwork();
     loadContentInPage();
   }
