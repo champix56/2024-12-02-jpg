787020fa097ba02eaf0f4066fe9d5c733d7662d0
champix56 <desorbaix@free.fr>
2024-12-04 15:32:27 +0100

    gestion url

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ function loadingDOM() {
  * Fonction de chargement des events de navbar
  */
 function loadNavbarEvents() {
-  var aList = document.querySelectorAll("nav a");
+  var aList = document.querySelectorAll("a");
   // for(var i=0;i<aList.length;i++){
   //   aList[i].addEventListener('click',function(evt){
   //     evt.preventDefault();
@@ -18,8 +18,9 @@ function loadNavbarEvents() {
   //   })
   // }
   aList.forEach(function (element) {
-    element.addEventListener("click", function (evt) {
+    element.addEventListener("click", (evt) => {
       evt.preventDefault();
+      history.pushState(null,null,evt.target.href);
     });
   });
 }
