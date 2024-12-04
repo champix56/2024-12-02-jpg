36ab101bd89055e46b496cff2eaa8d92e76af1ea
champix56 <desorbaix@free.fr>
2024-12-04 11:39:39 +0100

    moddif du contenu dom

--- js/index.js
+++ js/index.js
@@ -1,1 +1,3 @@
-alert("Le fichier js est chargé");
+var js=document.querySelector('#js-notification');
+js.style.backgroundColor='GREEN';
+js.innerHTML='le <span style="font-weight:900;color :blue;">JS</span> est OK';
