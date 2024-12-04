5d8e26403ecaab998c965d0701158a525980e7a6
champix56 <desorbaix@free.fr>
2024-12-04 15:03:56 +0100

    JSDOC + event de navbar

--- js/index.js
+++ js/index.js
@@ -1,6 +1,15 @@
+/**
+ * chargement de base de mon app 
+ */
 function loadingDOM() {
   document.querySelector("#js-notification").remove();
   document.querySelector("#header button").remove();
+  loadNavbarEvents();
 }
+/**
+ * Fonction de chargement des events de navbar
+ */
+function loadNavbarEvents(){
 
+}
 document.addEventListener("DOMContentLoaded", loadingDOM);
