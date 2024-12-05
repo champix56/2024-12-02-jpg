b31e70079605712b90f5136fee469db4184a3295
champix56 <desorbaix@free.fr>
2024-12-06 00:52:04 +0100

    demo avec template

--- js/web-components/horloge.js
+++ js/web-components/horloge.js
@@ -1,4 +1,9 @@
 export class Horloge extends HTMLElement {
+    constructor(){
+        super();
+        const templateClone = document.querySelector('#horloge-template').content.children[0].cloneNode(true);
+        this.appendChild(templateClone);
+    }
   // The browser calls this method when the element is
   // added to the DOM.
   connectedCallback() {
@@ -8,7 +13,7 @@ export class Horloge extends HTMLElement {
 
       // Format the date to a human-friendly string, and set the
       // formatted date as the text content of this element.
-      this.innerHTML ='<a href="https://fr.piliapp.com/time">'+ now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()+"</a>";
+      this.querySelector('a').innerHTML = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds();
     }, 1000);
   }
 }
