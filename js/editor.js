88b7da62d3951ddb069e976cfb3f245f0554d303
champix56 <desorbaix@free.fr>
2024-12-05 17:12:30 +0100

    chargement du select sur promise<Images>

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,9 @@
-function loadEditor(params){
+function loadEditor(params) {
   console.log(params);
   loadEditorEvent();
+  promiseImage.then((arrayImages) => {
+    loadSelectImagesInForm(arrayImages);
+  });
 }
 
 function loadEditorEvent() {
@@ -9,3 +12,20 @@ function loadEditorEvent() {
     console.log("form submit");
   });
 }
+/**
+ *
+ * @param {Images} images
+ */
+const loadSelectImagesInForm = (images) => {
+  const select = document.forms["editor-form"]["imageId"];
+  const optionBase = select.children[0];
+  select.innerHTML = "";
+  select.appendChild(optionBase);
+  images.forEach((image) => {
+    const optionClone = optionBase.cloneNode(true);
+    optionClone.value = image.id;
+    optionClone.innerHTML = image.name;
+    select.appendChild(optionClone);
+  });
+  // debugger;
+};
