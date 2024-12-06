b44e2e690aeaf9230aceac6226cb009df1599992
champix56 <desorbaix@free.fr>
2024-12-06 10:50:49 +0100

    gestion image SVG

--- js/editor.js
+++ js/editor.js
@@ -85,10 +85,19 @@ const loadSelectImagesInForm = (images) => {
  * @param {HTMLElement} svgRootNode noeud svg pour affichage
  */
 const updateSVG = (meme, svgRootNode) => {
+  const img = images.find((i) => {
+    return i.id === meme.imageId;
+  });
   //1. svg viewbox
-
+  svgRootNode.setAttribute(
+    "viewBox",
+    `0 0 ${undefined !== img ? img.w : "500"} ${
+      undefined !== img ? img.h : "500"
+    }`
+  );
+  svgRootNode.innerHTML = "";
   //2.selection a partir de svgRootNode -> text
-  const text = svgRootNode.querySelector("text");
+  const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
   text.innerHTML = meme.text;
   //mise a jour des attributs de text
   text.setAttribute("x", meme.x);
@@ -96,7 +105,22 @@ const updateSVG = (meme, svgRootNode) => {
   text.setAttribute("font-size", meme.fontSize);
   text.setAttribute("font-weight", meme.fontWeight);
   text.setAttribute("fill", meme.color);
-  text.setAttribute("text-decoration", meme.underline?'underline':'none');
-  text.setAttribute("font-style", meme.italic?'italic':'normal');
+  text.setAttribute("text-decoration", meme.underline ? "underline" : "none");
+  text.setAttribute("font-style", meme.italic ? "italic" : "normal");
+  svgRootNode.appendChild(text);
 
+  if (undefined !== img) {
+    const imageSVG = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "image"
+    );
+    imageSVG.setAttribute("x", 0);
+    imageSVG.setAttribute("y", 0);
+    /*const href=document.createAttributeNS("http://www.w3.org/1999/xlink",'xlink:href');
+    href.value=img.url;
+    imageSVG.setAttributeNodeNS(href);*/
+    imageSVG.setAttributeNS("http://www.w3.org/1999/xlink","xlink:href", img.url);
+
+    svgRootNode.insertBefore(imageSVG, text);
+  }
 };
